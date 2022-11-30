470dfd13682bd661715bbe6ab6de7afeb034713c
takuyadev <takuya.k.toyokawa@protonmail.com>
2022-11-29 23:31:29 -0800

    Fixed naming error

--- components/molecules/text/HeadingUlr.jsx
+++ components/molecules/text/HeadingUlr.jsx
@@ -8,47 +8,43 @@ import Heading2 from "@/atoms/text/Heading2"
 
 // Styled Components
 const Container = styled.div`
-   display: flex;
-   align-items: center;
-   gap: 1em;
+  display: flex;
+  align-items: center;
+  gap: 1em;
 `
 
 const TitleContainer = styled.div``
 
 const StyledHeading = styled(Heading2)`
-   margin-bottom: 0.1em;
+  margin-bottom: 0.1em;
 `
 
 const IconContainer = styled.div`
-   width: 40px;
-   height: 40px;
+  width: 40px;
+  height: 40px;
 `
 
 // Component
 const HeadingUlr = forwardRef(({ children, icon }, ref) => {
-   return (
-      <Container>
-         {icon &&
-            <IconContainer>
-               {icon}
-            </IconContainer>
-         }
-         <TitleContainer>
-            <StyledHeading>{children}</StyledHeading>
-            <Image
-               width={150}
-               height={10}
-               src="/img/squiggly.png"
-               alt="Vector of squiggly lines"
-            />
-         </TitleContainer>
-      </Container>
-   )
+  return (
+    <Container>
+      {icon && <IconContainer>{icon}</IconContainer>}
+      <TitleContainer>
+        <StyledHeading>{children}</StyledHeading>
+        <Image
+          width={150}
+          height={10}
+          src="/img/other/squiggly.webp"
+          alt="Vector of squiggly lines"
+        />
+      </TitleContainer>
+    </Container>
+  )
 })
 
 // Settings & Exports
 HeadingUlr.defaultProps = {
-   children: "Heading"
+  children: "Heading",
 }
 
 HeadingUlr.displayName = "HeadingUlr"
