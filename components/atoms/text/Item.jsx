b658d62b2b3a20ad25d8d39397c79764840e8d73
takuyadev <takuya.k.toyokawa@protonmail.com>
2022-12-05 17:43:22 -0800

    Update Item.jsx

--- components/atoms/text/Item.jsx
+++ components/atoms/text/Item.jsx
@@ -5,6 +5,7 @@ import styled from "styled-components"
 // Styled Components
 const StyledItem = styled.li`
   line-height: 175%;
+  color: ${({ theme }) => theme.light.main};
 `
 
 const Item = forwardRef(({ children }, ref) => (
